04996bc65b3c75e3416fbfb89a4f9e4c615d87ba
Waruna <waruna@Warunas-MacBook-Pro.local>
2022-04-21 14:43:06 -0700

    heroku

--- src/app.js
+++ src/app.js
@@ -24,14 +24,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Waruna Perera'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Waruna Perera'
     })
 })
 
@@ -39,7 +39,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Waruna Perera'
     })
 })
 
@@ -94,7 +94,7 @@ app.get('/product', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Waruna Perera',
         errorMessage: 'Help article not found.'
     })
 })
@@ -102,7 +102,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Waruna Perera',
         errorMessage: 'Page not found.'
     })
 })
